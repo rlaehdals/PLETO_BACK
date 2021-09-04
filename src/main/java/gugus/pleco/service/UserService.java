134235a22e8c42539b23dca593aebdaff430a57e
rlaehdals <rkdlem48@gmail.com>
2021-09-04 14:33:17 +0900

    .

--- src/main/java/gugus/pleco/service/UserService.java
+++ src/main/java/gugus/pleco/service/UserService.java
@@ -5,13 +5,14 @@ import javassist.bytecode.DuplicateMemberException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import gugus.pleco.controller.dto.UserDto;
 import gugus.pleco.domain.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService extends UserDetailsService {
 
     User join(UserDto userDto) throws UserDupulicatedException;
 
 
-    User login(UserDto userDto);
+    User login(UserDto userDto) throws UsernameNotFoundException;
 
     User findById(Long id);
 
