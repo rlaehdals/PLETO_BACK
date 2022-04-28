4d9ae9c00f6939b4794c03493a8517aed3a0045c
rlaehdals <rkdlem48@gmail.com>
2022-04-28 14:53:41 +0900

    fix controlleradvice 패지키 수정

--- src/main/java/gugus/pleco/util/controller/advice/ControllerAdvice.java
+++ src/main/java/gugus/pleco/util/controller/advice/ControllerAdvice.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = "gugus.pleco.controller")
+@RestControllerAdvice(basePackages = "gugus.pleco.domain")
 public class ControllerAdvice {
 
     @ExceptionHandler
